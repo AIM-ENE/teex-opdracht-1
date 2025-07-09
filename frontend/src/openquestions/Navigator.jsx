3518e582c57eb43ee73c95eca1e758398c280d94
Rody Middelkoop <rody.middelkoop@han.nl>
2025-07-09 12:16:59 +0200

    Navigator een flexibele toestand gegeven

--- frontend/src/openquestions/Navigator.jsx
+++ frontend/src/openquestions/Navigator.jsx
@@ -1,15 +1,44 @@
+import React, { useState, useEffect } from "react";
 import NavigationButton from "./NavigationButton";
-import React from "react";
 
 export default function Navigator({ currentQuestionId, next, previous }) {
-  if (currentQuestionId === 1) {
+    const [state, setState] = useState(new StartState());
+
+    useEffect(() => {
+        setState((currentState) => currentState.nextState(currentQuestionId));
+    }, [currentQuestionId]);
+
+    return state.render(previous, next);
+}
+
+class StartState {
+  nextState(currentQuestionId) {
+    if (currentQuestionId >= 2 && currentQuestionId < 4) {
+      return new MiddleState();
+    }
+    return this;
+  }
+
+  render(previous, next) {
     return (
       <div className="navigation">
         <NavigationButton action={next} title="Volgende" />
       </div>
     );
   }
-  if (currentQuestionId >= 2 && currentQuestionId < 4) {
+}
+
+class MiddleState {
+  nextState(currentQuestionId) {
+    if (currentQuestionId === 4) {
+      return new EndState();
+    } else if (currentQuestionId === 1) {
+      return new StartState();
+    }
+    return this;
+  }
+
+  render(previous, next) {
     return (
       <div className="navigation">
         <NavigationButton action={previous} title="Vorige" />
@@ -17,7 +46,17 @@ export default function Navigator({ currentQuestionId, next, previous }) {
       </div>
     );
   }
-  if (currentQuestionId === 4) {
+}
+
+class EndState {
+  nextState(currentQuestionId) {
+    if (currentQuestionId >= 2 && currentQuestionId < 4) {
+      return new MiddleState();
+    }
+    return this;
+  }
+
+  render(previous) {
     return (
       <div className="navigation">
         <NavigationButton action={previous} title="Vorige" />
